751e68faab898cad3098ce212809348b8eaf5cad
AntonioAlgarin <antonio@cybermindedvets.com>
2022-11-03 23:03:46 -0700

    highgarden begins

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1>Welcome to Highgarden</h1>
+    </React.Fragment>
+  );
+}
+
+export default App;
