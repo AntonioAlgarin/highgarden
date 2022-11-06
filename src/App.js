64e3fb10ff56699ad21f763f18fabe3ba308f021
AntonioAlgarin <antonio@cybermindedvets.com>
2022-11-05 18:31:34 -0700

    highgarden cont

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <React.Fragment>
       <h1>Welcome to Highgarden</h1>
       <h2>This is where I will add plants</h2>
+      <h3>Click here to view more</h3>
     </React.Fragment>
   );
 }
