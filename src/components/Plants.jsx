25ac47637e3368dbfe85d55401bc90070d4ffbdf
AntonioAlgarin <antonio@cybermindedvets.com>
2022-11-08 15:52:46 -0800

    changed App.js to App.jsx, added components folder

---
+++ src/components/Plants.jsx
@@ -0,0 +1,14 @@
+import "./plant.css";
+import React from "react";
+
+function Plant() {
+  return (
+    <React.Fragment>
+      <h1>This is where I will display my plants</h1>
+      <h2>Toggle watered yes or no</h2>
+      <button>Click here to add new plant</button>
+    </React.Fragment>
+  );
+}
+
+export default App;
